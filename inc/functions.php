6a761dd7c4669ab48c19bcd643880e4cda63b491
Ju4n-tamby <juanalbertinokiller@gmail.com>
2025-06-25 08:53:25 +0300

    new

---
+++ inc/functions.php
@@ -0,0 +1,29 @@
+<?php
+require("connection.php");
+session_start();
+ini_set('display_errors', 1);
+
+function getAlldepartements()
+{
+    $sql = "SELECT * FROM departments";
+    $news_req = mysqli_query(dbconnect(), $sql);
+    $departments = array();
+    while ($result = mysqli_fetch_assoc($news_req)) {
+        $departments[] = $result;
+    }
+    mysqli_free_result($news_req);
+    return $departments;
+}
+
+function getAllManagersNow($dept_no){
+    $sql = "SELECT * FROM dept_manager JOIN employees ON dept_manager.emp_no = employees.emp_no WHERE dept_no='$dept_no' AND from_date <= NOW() AND to_date >= NOW()";
+    $news_req = mysqli_query(dbconnect(), $sql);
+    $managers = array();
+    while ($result = mysqli_fetch_assoc($news_req)) {
+        $managers[] = $result;
+    }
+    mysqli_free_result($news_req);
+    return $managers;
+}
+
+?>
